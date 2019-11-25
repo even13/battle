5c581ca53b1e6198ae12566210ccfa31ed57f2bb
Student <student@Makerss-MacBook-Air.local>
2019-11-25 15:55:24 +0000

    add cat form view and post request method

--- app.rb
+++ app.rb
@@ -11,8 +11,12 @@ get '/random_cat' do
   erb(:index)
 end
 
-get '/named_cat' do
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
 end
